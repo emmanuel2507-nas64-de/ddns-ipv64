5d4ee67d463c0ab1b4dd47fcc93c5d65c28bca82
emmanuel2507-nas64-de <emmanuel.larry25.07@gmail.com>
2025-09-03 19:06:40 +0200

    domain-ip-scheck.sh

--- data/domain-ip-scheck.sh
+++ data/domain-ip-scheck.sh
@@ -3,9 +3,8 @@
 DATUM=$(date +%Y-%m-%d\ %H:%M:%S)
 
 if [[ "$NETWORK_CHECK" =~ (YES|yes|Yes) ]] ; then
-    # if ! curl -4sf --user-agent "${CURL_USER_AGENT}" "https://ipv64.net" 2>&1 > /dev/null; then
-    if ! curl -4sf --user-agent "${CURL_USER_AGENT}" "https://ipv64.net/ipcheck.php" 2>&1 > /dev/null; then
-        echo "$DATUM  FEHLER !!!  - 404 Sie haben kein Netzwerk oder Internetzugang oder die Webseite ipv64.net ist nicht erreichbar"
+    if ! curl -6sf --user-agent "${CURL_USER_AGENT}" "https://ipv64.net/ipcheck.php" 2>&1 > /dev/null; then
+        echo "$DATUM  ERROR !!!  - 404 You have no network or internet access or the website ipv64.net is not reachable"
         echo "=============================================================================================="
         exit 1
     fi
@@ -13,7 +12,7 @@ if [[ "$NETWORK_CHECK" =~ (YES|yes|Yes) ]] ; then
     NAMESERVER_CHECK=$(dig +timeout=1 @${NAME_SERVER} 2>/dev/null)
     echo "$NAMESERVER_CHECK" | grep -s -q "timed out" && { NAMESERVER_CHECK="Timeout" ; STATUS="FAIL" ; }
     if [ "${STATUS}" = "FAIL" ] ; then
-        echo "$DATUM  FEHLER !!!  - 404 NAMESERVER ${NAME_SERVER} ist nicht ist nicht erreichbar. Sie haben kein Netzwerk oder Internetzugang"
+        echo "$DATUM  ERROR !!!  - 404 NAMESERVER ${NAME_SERVER} is not reachable. You have no network or internet access"
         echo "=============================================================================================="
         exit 1
     fi
@@ -23,11 +22,11 @@ fi
 
 if [[ "${DOMAIN_PRAEFIX_YES}" =~ (YES|yes|Yes) ]] ; then
     for DOMAIN in $(echo "${DOMAIN_IPV64}" | sed -e "s/,/ /g"); do
-        echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Deine DOMAIN mit PRAEFIX ${DOMAIN_PRAEFIX}.${DOMAIN} HAT DIE IP=`dig +short ${DOMAIN_PRAEFIX}.${DOMAIN} A @${NAME_SERVER}`" >> /data/log/cron.log 2>&1
+        echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Your DOMAIN with PREFIX ${DOMAIN_PRAEFIX}.${DOMAIN} HAS THE IP=`dig +short ${DOMAIN_PRAEFIX}.${DOMAIN} A @${NAME_SERVER}`" >> /data/log/cron.log 2>&1
     done
 else
     for DOMAIN in $(echo "${DOMAIN_IPV64}" | sed -e "s/,/ /g"); do
-        echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Deine DOMAIN ${DOMAIN} HAT DIE IP=`dig +short ${DOMAIN} A @${NAME_SERVER}`" >> /data/log/cron.log 2>&1
+        echo "`date +%Y-%m-%d\ %H:%M:%S`  IP CHECK    - Your DOMAIN ${DOMAIN} HAS THE IP=`dig +short ${DOMAIN} A @${NAME_SERVER}`" >> /data/log/cron.log 2>&1
     done
 fi
 echo "=============================================================================================="
